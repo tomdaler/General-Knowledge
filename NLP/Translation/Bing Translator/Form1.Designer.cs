afc680f134a174c43b6852d029d68e69ca03da50
tomdaler <101162223+tomdaler@users.noreply.github.com>
2022-04-07 10:50:12 -0600

    Add files via upload

---
+++ NLP/Translation/Bing Translator/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Bing_Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.txtTranslatedText = new System.Windows.Forms.TextBox();
+            this.txtTraslateFrom = new System.Windows.Forms.TextBox();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.cb2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(6, 130);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(84, 13);
+            this.Label2.TabIndex = 9;
+            this.Label2.Text = "Translated Text:";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(6, 7);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(80, 13);
+            this.Label1.TabIndex = 8;
+            this.Label1.Text = "Translate From:";
+            // 
+            // txtTranslatedText
+            // 
+            this.txtTranslatedText.Location = new System.Drawing.Point(6, 154);
+            this.txtTranslatedText.Multiline = true;
+            this.txtTranslatedText.Name = "txtTranslatedText";
+            this.txtTranslatedText.Size = new System.Drawing.Size(369, 92);
+            this.txtTranslatedText.TabIndex = 7;
+            // 
+            // txtTraslateFrom
+            // 
+            this.txtTraslateFrom.Location = new System.Drawing.Point(6, 29);
+            this.txtTraslateFrom.Multiline = true;
+            this.txtTraslateFrom.Name = "txtTraslateFrom";
+            this.txtTraslateFrom.Size = new System.Drawing.Size(369, 92);
+            this.txtTraslateFrom.TabIndex = 6;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(6, 254);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(121, 24);
+            this.btnTranslate.TabIndex = 5;
+            this.btnTranslate.Text = "Traslate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // cb1
+            // 
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Location = new System.Drawing.Point(92, 4);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(99, 21);
+            this.cb1.TabIndex = 10;
+            // 
+            // cb2
+            // 
+            this.cb2.FormattingEnabled = true;
+            this.cb2.Location = new System.Drawing.Point(92, 127);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(99, 21);
+            this.cb2.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(391, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(318, 306);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(716, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 39);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Read C:\\Translate.cvs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(715, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 24);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Traslate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(715, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 24);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Export";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 402);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.cb1);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.txtTranslatedText);
+            this.Controls.Add(this.txtTraslateFrom);
+            this.Controls.Add(this.btnTranslate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox txtTranslatedText;
+        internal System.Windows.Forms.TextBox txtTraslateFrom;
+        internal System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.ComboBox cb2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        internal System.Windows.Forms.Button button2;
+        internal System.Windows.Forms.Button button3;
+
+    }
+}
+
---
+++ NLP/Translation/Bing Translator/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Bing_Translator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            txtTranslatedText.Text = Traducir(1, txtTraslateFrom.Text);
+        }
+
+
+        private string Traducir(int opcion, string fromText)
+        {
+            string from1 = "en";
+            string to1 = "en";
+            string strTranslatedText = null;
+
+            if (cb1.Text == "Spanish") from1 = "es";
+            if (cb1.Text == "Portuguese") from1 = "pt";
+            if (cb1.Text == "German") from1 = "de";
+            if (cb1.Text == "French") from1 = "fr";
+            if (cb1.Text == "Arab") from1 = "ar";
+            if (cb1.Text == "Chinesse") from1 = "zh-CHS";
+
+            if (cb2.Text == "Spanish") to1 = "es";
+            if (cb2.Text == "Portuguese") to1 = "pt";
+            if (cb2.Text == "German") to1 = "de";
+            if (cb2.Text == "French") to1 = "fr";
+            if (cb2.Text == "Arab") to1 = "ar";
+            if (cb2.Text == "Chinesse") to1 = "zh-CHS";
+
+
+            try {
+                   TranslatorService.LanguageServiceClient client = new TranslatorService.LanguageServiceClient();
+                   client = new TranslatorService.LanguageServiceClient();
+                   strTranslatedText = client.Translate("6CE9C85A41571C050C379F60DA173D286384E0F2", fromText,  from1, to1);
+                   return strTranslatedText;
+
+                     //en english
+                     //de german
+                     //es espanol
+                     //ar arab
+                     //fr french
+                     //pt portuguese
+                     //zh-CHS  chinesse simplied traditions
+
+                     //translationResult = client.Translate("", sourceText, "en", "de", "text/html", "general");
+                  }
+             catch (Exception ex) {
+                   MessageBox.Show(ex.Message);
+                 }
+            return "";
+        }
+
+        #region Readfile
+        private DataTable ReadFile()
+        {
+            string sql2 = "Select * FROM [Translate.csv]";
+           
+            string strPath = "c:\\";
+            string Cn1 = "Driver={Microsoft Text Driver (*.txt; *.csv)}; DefaultDir=" + strPath;
+
+            System.Data.Odbc.OdbcDataAdapter da = new OdbcDataAdapter(sql2, Cn1);
+            DataSet ds = new DataSet();
+
+            try
+            {
+                da.Fill(ds, "Files");            
+                dataGridView1.Visible = true;           
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return null;
+            }
+            return ds.Tables[0];
+           
+        }
+        #endregion
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cb1.Items.Add("English");
+            cb1.Items.Add("Spanish");
+            cb1.Items.Add("Portuguese");
+            cb1.Items.Add("German");
+            cb1.Items.Add("French");
+            cb1.Items.Add("Arab");
+            cb1.Items.Add("Chinesse");
+
+            cb2.Items.Add("English");
+            cb2.Items.Add("Spanish");
+            cb2.Items.Add("Portuguese");
+            cb2.Items.Add("German");
+            cb2.Items.Add("French");
+            cb2.Items.Add("Arab");
+            cb2.Items.Add("Chinesse");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ReadFile();
+            dataGridView1.Visible = true;
+            button2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int conteo = 0;
+            DataTable dt = ReadFile();
+            foreach (DataRow dr in dt.Rows)
+            {
+                string ss = Traducir(2, dr[0].ToString());
+                dataGridView1.Rows[conteo].Cells[1].Value = ss.ToString();
+                conteo++;
+
+               // string ss2;
+              //  if ((conteo % 20) == 0)
+              //  {
+              //     MessageBox.Show("Message","Seguir"); //
+
+               // }
+
+            }
+            button3.Visible = true;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TextWriter sw = new StreamWriter(@"C:\\Translation.txt"); 
+            int rowcount = dataGridView1.Rows.Count; 
+            for (int i = 0; i < rowcount - 1; i++) 
+            { 
+                sw.WriteLine(dataGridView1.Rows[i].Cells[0].Value.ToString() + "\t" + dataGridView1.Rows[i].Cells[1].Value.ToString() + "\t" + dataGridView1.Rows[i].Cells[2].Value.ToString());
+            }
+            
+            sw.Close();     
+            //Don't Forget Close the TextWriter Object(sw)   
+            MessageBox.Show("Data Successfully Exported");
+        }
+    }
+}
